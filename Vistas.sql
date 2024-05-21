3eb353df9f9789c840e6af1e268c35e07b620c64
kevin-luna <L21020397@veracruz.tecnm.mx>
2024-05-21 15:20:50 -0600

    Se agregaron las primeras 2 vistas para generar reportes.

--- Vistas.sql
+++ Vistas.sql
@@ -6,3 +6,16 @@ CREATE VIEW direccion as
 
 CREATE VIEW tipo_lote as
     SELECT CA_CLAVE as clave_catalogo ,CA_DESCRIPCION as descripcion, CA_IMPORTE as importe ,CON_CLAVE as clave_contab FROM Catalogo_COL WHERE CA_TIPO = 2;
+
+
+-- Esta vista se usa para la primera consulta personalizada
+CREATE VIEW reporte_cargos
+SELECT Clientes.CL_NUMERO as Cliente, Clientes.CL_NOM as Nombre, Clientes.CL_DIREC as Direccion, SUM(CARGOS.CAR_IMPORTE) as Cargo_total
+    FROM Clientes
+    JOIN CARGOS ON Clientes.CL_NUMERO = CARGOS.CL_NUMERO
+    GROUP BY Clientes.CL_NUMERO;
+
+CREATE VIEW reporte_lotes
+SELECT Colono_Lote.CL_NUMERO as Cliente, Clientes.CL_NOM as Nombre, Colono_Lote.L_MANZANA as Manzana, Colono_Lote.L_NUMERO as Lote  FROM Colono_Lote
+    JOIN Clientes ON Clientes.CL_NUMERO = Colono_Lote.CL_NUMERO
+    ORDER BY Nombre;
