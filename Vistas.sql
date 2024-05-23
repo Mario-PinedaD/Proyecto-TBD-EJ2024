c87a3e274dcb481881617e27f8a620c70a21ca58
kevin-luna <L21020397@veracruz.tecnm.mx>
2024-05-23 01:11:56 -0600

    Se crearon los procedimientos para realizar las consultas solicitadas.

--- Vistas.sql
+++ Vistas.sql
@@ -7,10 +7,8 @@ CREATE VIEW direccion as
 CREATE VIEW tipo_lote as
     SELECT CA_CLAVE as clave_catalogo ,CA_DESCRIPCION as descripcion, CA_IMPORTE as importe ,CON_CLAVE as clave_contab FROM Catalogo_COL WHERE CA_TIPO = 2;
 
-
--- Esta vista se usa para la primera consulta personalizada
 CREATE VIEW reporte_cargos as
-SELECT Clientes.CL_NUMERO as Cliente, Clientes.CL_NOM as Nombre, Clientes.CL_DIREC as Direccion, SUM(CARGOS.CAR_IMPORTE) as Cargo_total
+SELECT Clientes.CL_NUMERO as Cliente, Clientes.CL_NOM as Nombre, Clientes.CL_DIREC as Direccion,CARGOS.CAR_FECHA as Fecha, SUM(CARGOS.CAR_IMPORTE) as Cargo_total
     FROM Clientes
     JOIN CARGOS ON Clientes.CL_NUMERO = CARGOS.CL_NUMERO
     GROUP BY Clientes.CL_NUMERO;
@@ -20,12 +18,48 @@ SELECT Colono_Lote.CL_NUMERO as Cliente, Clientes.CL_NOM as Nombre, Colono_Lote.
     JOIN Clientes ON Clientes.CL_NUMERO = Colono_Lote.CL_NUMERO
     ORDER BY Nombre;
 
-CREATE VIEW reporte_cargos_fecha as
-SELECT CAR_FECHA as Fecha ,Clientes.CL_NUMERO as Cliente,CAR_IMPORTE as Importe FROM CARGOS
-    JOIN Clientes ON Clientes.CL_NUMERO = CARGOS.CL_NUMERO;
-
 CREATE VIEW reporte_importe_status as
 SELECT Catalogo_COL.CA_DESCRIPCION AS Status,SUM(CARGOS.CAR_IMPORTE) AS Importe_Total FROM Catalogo_COL
     JOIN Lote ON Catalogo_COL.CA_CLAVE = Lote.CA_CLAVE0
     JOIN CARGOS ON Lote.L_MANZANA = CARGOS.L_MANZANA AND Lote.L_NUMERO = CARGOS.L_NUMERO
     GROUP BY Catalogo_COL.CA_TIPO, Catalogo_COL.CA_DESCRIPCION;
+
+CREATE VIEW reporte_cargos_fecha as
+SELECT CAR_FECHA as Fecha ,Clientes.CL_NUMERO as Cliente,CAR_IMPORTE as Importe FROM CARGOS
+    JOIN Clientes ON Clientes.CL_NUMERO = CARGOS.CL_NUMERO
+    ORDER BY Fecha;
+DELIMITER $$
+
+CREATE PROCEDURE obtener_reporte_cargos(IN P_FECHA date)
+BEGIN
+    IF ISNULL(P_FECHA) THEN
+        SELECT * FROM reporte_cargos;
+    ELSE
+        SELECT * FROM reporte_cargos WHERE Fecha = P_FECHA;
+    END IF;
+END$$
+
+CREATE PROCEDURE obtener_reporte_lotes(IN P_CLIENTE double)
+BEGIN
+    IF ISNULL(P_CLIENTE) THEN
+        SELECT * FROM reporte_lotes;
+    ELSE
+        SELECT * FROM reporte_lotes WHERE Cliente = P_CLIENTE;
+    END IF;
+END$$
+
+CREATE PROCEDURE obtener_importe_status()
+BEGIN
+    SELECT * FROM reporte_importe_status;
+END$$
+
+CREATE PROCEDURE obtener_importe_cargos_fecha(IN P_FECHA_INICIO date,IN P_FECHA_FIN date)
+BEGIN
+    IF ISNULL(P_FECHA_INICIO) OR  isnull(P_FECHA_FIN) THEN
+        SELECT * FROM reporte_lotes;
+    ELSE
+        SELECT * FROM reporte_cargos_fecha WHERE Fecha BETWEEN P_FECHA_INICIO AND P_FECHA_FIN;
+    END IF;
+END$$
+
+DELIMITER ;
