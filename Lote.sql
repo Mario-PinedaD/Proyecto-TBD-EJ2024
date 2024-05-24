e3a4c1c84d1285a21a575ec3f5655daadd191447
kevin-luna <L21020397@veracruz.tecnm.mx>
2024-05-23 22:24:01 -0600

    Se corrigieron errores en los scripts Lote y Colono_Lote.sql

--- Lote.sql
+++ Lote.sql
@@ -1,3 +1,6 @@
+SET GLOBAL log_bin_trust_function_creators = 1;
+
+DELIMITER $$
 -- Funciones, Triggers y Procedimientos para la tabla LOTE
 
 -- Función para verificar la existencia de un registro en LOTE
@@ -49,8 +52,7 @@ BEGIN
 END $$
 
 -- Trigger antes de eliminar en LOTE
-DROP TRIGGER IF EXISTS Antes_Actualizar_LOTE $$
-
+DROP TRIGGER IF EXISTS Antes_Eliminar_LOTE $$
 CREATE TRIGGER Antes_Eliminar_LOTE
 BEFORE DELETE ON LOTE
 FOR EACH ROW
@@ -109,8 +111,9 @@ BEGIN
     -- Intentar la inserción
      INSERT INTO Lote (L_MANZANA, L_NUMERO, CA_CLAVE1, CA_CLAVE2, CA_CLAVE0, L_TOTAL_M2,
                        L_IMPORTE, L_NUM_EXT, L_PAHT_MAP, L_FECHA_MANT, L_FECHA_POSIBLE)
-        VALUES (p_L_MANZANA, p_L_NUMERO, p_CA_CLAVE1, p_CA_CLAVE2, p_CA_CLAVE0, p_L_TOTAL_M2,
+        VALUES (p_L_MANZANA, p_L_NUMERO, p_L_CA_CLAVE1, p_L_CA_CLAVE2, p_L_CA_CLAVE0, p_L_TOTAL_M2,
                 p_L_IMPORTE, p_L_NUM_EXT, p_L_PAHT_MAP, p_L_FECHA_MANT, p_L_FECHA_POSIBLE);
+    END IF;
 END $$
 
 -- Procedimiento almacenado para eliminar en LOTE
@@ -125,7 +128,8 @@ BEGIN
     IF p_L_MANZANA IS NULL OR p_L_NUMERO IS NULL THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'L_MANZANA y L_NUMERO no pueden ser nulos';
     ELSE
-    DELETE FROM LOTE WHERE L_MANZANA = p_L_MANZANA AND L_NUMERO = p_L_NUMERO;
+        DELETE FROM LOTE WHERE L_MANZANA = p_L_MANZANA AND L_NUMERO = p_L_NUMERO;
+    END IF;
 END $$
 
 -- Procedimiento almacenado para buscar en LOTE
@@ -140,13 +144,14 @@ BEGIN
     IF p_L_MANZANA IS NULL OR p_L_NUMERO IS NULL THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'L_MANZANA y L_NUMERO no pueden ser nulos';
     ELSE
-    IF p_L_MANZANA IS NULL OR p_L_NUMERO IS NULL THEN
-        SELECT * FROM LOTE;
-    ELSE
-        IF Existe_LOTE(p_L_MANZANA, p_L_NUMERO) THEN
-            SELECT * FROM LOTE WHERE L_MANZANA = p_L_MANZANA AND L_NUMERO = p_L_NUMERO;
+        IF p_L_MANZANA IS NULL OR p_L_NUMERO IS NULL THEN
+            SELECT * FROM LOTE;
         ELSE
-            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El registro no existe en LOTE';
+            IF Existe_LOTE(p_L_MANZANA, p_L_NUMERO) THEN
+                SELECT * FROM LOTE WHERE L_MANZANA = p_L_MANZANA AND L_NUMERO = p_L_NUMERO;
+            ELSE
+                SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El registro no existe en LOTE';
+            END IF;
         END IF;
     END IF;
 END $$
@@ -172,20 +177,22 @@ BEGIN
     IF p_L_MANZANA IS NULL OR p_L_NUMERO IS NULL THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'L_MANZANA y L_NUMERO no pueden ser nulos';
     ELSE
-    -- Intentar la actualización
-    UPDATE LOTE
-    SET
-        L_MANZANA = p_L_MANZANA,
-        L_NUMERO = p_L_NUMERO,
-        CA_CLAVE1 = p_CA_CLAVE1,
-        CA_CLAVE2 = p_CA_CLAVE2,
-        CA_CLAVE0 = p_CA_CLAVE0,
-        L_TOTAL_M2 = p_L_TOTAL_M2,
-        L_IMPORTE = p_L_IMPORTE,
-        L_NUM_EXT = p_L_NUM_EXT,
-        L_PAHT_MAP = p_L_PAHT_MAP,
-        L_FECHA_MANT = p_L_FECHA_MANT,
-        L_FECHA_POSIBLE = L_FECHA_POSIBLE
+        -- Intentar la actualización
+        UPDATE LOTE
+        SET
+            L_MANZANA = p_L_MANZANA,
+            L_NUMERO = p_L_NUMERO,
+            CA_CLAVE1 = p_L_CA_CLAVE1,
+            CA_CLAVE2 = p_L_CA_CLAVE2,
+            CA_CLAVE0 = p_L_CA_CLAVE0,
+            L_TOTAL_M2 = p_L_TOTAL_M2,
+            L_IMPORTE = p_L_IMPORTE,
+            L_NUM_EXT = p_L_NUM_EXT,
+            L_PAHT_MAP = p_L_PAHT_MAP,
+            L_FECHA_MANT = p_L_FECHA_MANT,
+            L_FECHA_POSIBLE = L_FECHA_POSIBLE
 
-    WHERE L_MANZANA = p_L_MANZANA AND L_NUMERO = p_L_NUMERO;
+        WHERE L_MANZANA = p_L_MANZANA AND L_NUMERO = p_L_NUMERO;
+    END IF;
 END $$
+DELIMITER ;
